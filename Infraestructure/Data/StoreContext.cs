ca6b8e72bf0567d36e179c5452e5e44fef1dd131
Sebastian R <josesrb10@outlook.com>
2022-07-31 21:27:16 -0600

    References and data context

---
+++ Infraestructure/Data/StoreContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infraestructure.Data
+{
+    public class StoreContext : DbContext
+    {
+
+        public StoreContext()
+            : base()
+        {
+        }
+        public StoreContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+    }
+}
